e3c505f929b7921168f29a4819044ecd6f12a13c
J Alfonso Martinez Baeza <mtz.alfonso96@gmail.com>
2020-10-11 19:04:03 -0500

    Add javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"simulador","l":"Administrador"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"simulador","l":"Main"},{"p":"simulador","l":"Memoria"},{"p":"simulador","l":"Proceso"}]
